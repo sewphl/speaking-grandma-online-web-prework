b663e66304e63fe438c53877fe46808e92fea447
sharon <sharonoptsout@gmail.com>
2018-09-13 22:20:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,8 @@ def speak_to_grandma(stuff)
     return "HUH?! SPEAK UP, SONNY!"
   elsif stuff == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif /[[:upper:]]/.match(stuff)
+  ##elsif /[[:upper:]]/.match(stuff)
+elsif stuff == stuff.upcase 
     return "NO, NOT SINCE 1938!"
 end
 # Write a speak_to_grandma method.
